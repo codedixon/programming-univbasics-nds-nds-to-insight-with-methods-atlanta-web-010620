449de0a4c4955e856aceae1551e0b5728a51a0e7
Rashad Dixon <dixoncodes@gmail.com>
2019-12-21 05:41:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,16 +28,18 @@ end
 def gross_for_director(director_data)
    director_index = 0
   result = {}
-  
+ pp director_data 
 #while director_index < director_data.length
  #movie_index = 0
 #director_name = director_data[director_index][:name] 
-result[director_name] = 0
- while movie_index < director_data[director_index][:movies].length
-  result[director_name] +=  director_data[director_index][:movies][movie_index][:worldwide_gross]
-  movie_index += 1
-end
-director_index += 1
-end
+#result[director_name] = 0
+ #while movie_index < director_data[director_index][:movies].length
+  #result[director_name] +=  director_data[director_index][:movies][movie_index][:worldwide_gross]
+  #movie_index += 1
+#end
+#director_index += 1
+#end
   return result
 end
+
+gross_for_director(director_data)
