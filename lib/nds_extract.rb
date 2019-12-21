edc2502f363b52f52bb7fe5d6de53d2502d48e8d
Rashad Dixon <dixoncodes@gmail.com>
2019-12-21 04:23:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,11 +26,9 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
- pp director_data
+# pp director_data
  # index = 0
 #  while index < director_data[:movies].length
  # director_data[index][:name] +=
 #  index += 1
 end
-
-gross_for_director(director_data)
